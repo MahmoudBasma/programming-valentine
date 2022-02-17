2e01a6c288573a722a375e1b072e4899b4c220b6
Mahmoud Basma <mahmoud.basma@lau.edu>
2022-02-17 03:17:42 +0200

    Final commit

--- app/src/main/java/com/example/programmingvalentine/MainActivity.java
+++ app/src/main/java/com/example/programmingvalentine/MainActivity.java
@@ -17,10 +17,11 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     Spinner spinner;
     ImageView imgView = null;
-    TextView name, res;
-    int[] percentages = new int[9];
-    boolean checked[] = new boolean[9];
+    TextView res, txtView;
+    int[] percentages = new int[10];
+    boolean checked[] = new boolean[10];
     Random rand = new Random();
+    TextView name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,12 +38,20 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     public void onItemSelected(AdapterView<?> parent, View view, int position, long l) {
         name = findViewById(R.id.name);
         imgView = findViewById(R.id.imageView);
-
+        txtView = findViewById(R.id.textView);
 
         String lang = parent.getItemAtPosition(position).toString();
         lang = lang.toLowerCase();
+
         percentages[position] = rand.nextInt(100);
         checked[position] = true;
+
+        if(name.getText() != ""){
+            txtView.setText(name.getText().toString() + "'s Result");
+        }
+        else{
+            txtView.setText("Loading Results ...");
+        }
 
         if(lang.equals("java")){
             imgView.setImageResource(R.drawable.java);
@@ -80,14 +89,34 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     }
 
     public void fillTable(AdapterView parent, int[] percentages){
+
         StringBuilder text = new StringBuilder();
         String lang;
         res = findViewById(R.id.results);
 
-        for(int i = 1; i < 9; i++){
+        for(int i = 1; i < 10; i++){
             lang = parent.getItemAtPosition(i).toString();
-            if(checked[i] && percentages[i]!= 0) text.append("love result of \t\t"+ lang + "\t\t is: " + percentages[i] + "\n");
+            for(int j = 0; j <= 16 - lang.length(); j++){
+                lang += " ";
+            }
+            if(checked[i] && percentages[i]!= 0) text.append("love result of \t\t\t\t" + stringfy(lang) + "\t\t is: "+percentages[i] + "\n");
         }
+
         res.setText(text.toString());
+    }
+
+    public String stringfy(String str){
+        StringBuilder stringBuilder = new StringBuilder();
+        if(str == "JavaScript"){
+            return "JavaScript";
+        }
+        if(str == "C#"){
+            str = str + "     \t\t\t";
+        }
+        for(int i = 0; i <= 15 - str.length(); i++){
+            stringBuilder.append(" ");
+        }
+        stringBuilder.append(str);
+        return stringBuilder.toString();
     }
 }
